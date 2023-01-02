d7aefc23a93b7502f1924b2256918c5d51191057
vinzvinci <vincent.villafuerte1@gmail.com>
2023-01-02 19:55:38 +0800

    chore: added cv

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -207,7 +207,7 @@ function Resume() {
           </li>
         ))}
       </ol>
-      <Button href="#" variant="secondary" className="group mt-6 w-full">
+      <Button href="./Villafuerte_Vincent_A_Resume.pdf" variant="secondary" className="group mt-6 w-full">
         Download CV
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
